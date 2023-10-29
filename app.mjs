4dabd534bb42a4dcd6308e9bc57558679a161c86
khadijakhalid24 <125192824+khadijakhalid24@users.noreply.github.com>
2023-10-29 17:06:47 -0400

    part 2, 3, and 4 completed. basic adding and filtering done

--- app.mjs
+++ app.mjs
@@ -20,11 +20,32 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
 
+  console.log(req.query); 
+  
+  const { semester, year, professor } = req.query;
   const queryObject = {};
+  // Check if semester, year, and professor parameters exist in the query
+  if (semester) {
+      queryObject.semester = semester;
+  }
+  if (professor) {
+      const regex = new RegExp(professor, 'i');
+      queryObject.professor = regex;
+  }
+
+  // Check if year parameter exists and perform a numeric partial search using regex
+  if (year) {     //TO-DO: PARTIAL SEARCH FOR YEAR
+    const numericYear = parseInt(year, 10); // Convert year to a number
+    if (!isNaN(numericYear)) {
+        queryObject.year = numericYear;
+    } else {
+        // Handle invalid input if year is not a number
+        return res.status(400).send('Invalid year input');
+    }
+  }
 
   try{
   const reviews = await Review.find(queryObject); 
-  console.log(reviews);
   res.render('reviews', { 'reviews': reviews });
   } catch (err) {
     console.log('Error retrieving reviews', err);
@@ -32,6 +53,28 @@ app.get('/', async (req, res) => {
   }
 
 }); 
+
+app.get('/reviews/add', (req, res) => {
+  res.render('add'); // Renders the addReview.hbs template
+});
+
+// Route handler to process form submission (POST request)
+app.post('/reviews/add', async (req, res) => {
+  // Extract form data from req.body
+  const { courseNumber, courseName, semester, year, professor, review } = req.body;
+
+  try {
+      // Create a new review in the database using the Review model
+      const numericYear = parseInt(year, 10); // Convert year to a number
+      await Review.create({ courseNumber, courseName, semester, numericYear, professor, review });
+
+      // Redirect back to the page that shows all reviews (e.g., '/')
+      res.redirect('/');
+  } catch (err) {
+      console.error('Error adding review:', err);
+      res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(process.env.PORT || 3000);  // to listen based on a configurable PORT number
 
